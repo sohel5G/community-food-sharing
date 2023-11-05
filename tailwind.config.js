262599d2939f42186c330f1c3f12c7a9d855120a
sohel5G <contactsohelrana@gmail.com>
2023-11-05 16:01:45 +0600

    header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ export default {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: { "defaultPrimaryColor": '#FA2371', "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#ff38116b", "400": "#60a5fa", "500": "#FF3811", "600": "#FF3811", "700": "#FF3811", "800": "#FF3811", "900": "#FF3811", "950": "#FF3811" }
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
